f2321379193ea9faca626db611d17e26e5239b9d
Paulo <paulo.barbosa.botelho@gmail.com>
2021-12-05 21:00:32 -0300

    Tarefa 4 - visao - concluida

---
+++ Visao/Tarefa_4/opencv.py
@@ -0,0 +1,101 @@
+import cv2
+import numpy as np
+
+def getpass():
+    pass
+
+raw_input = input  # Python 3
+
+alpha = 0.7
+
+# [blend_images]
+beta = (1.0 - alpha)
+
+def empty(a):
+    pass
+
+
+def stackImages(scale, imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range(0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape[:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]),
+                                                None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y] = cv2.cvtColor(imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank] * rows
+        hor_con = [imageBlank] * rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None, scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor = np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 63, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 123, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 125, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 251, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 63, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+frameWidth = 640
+frameHeight = 480
+cap = cv2.VideoCapture(0)
+cap.set(3, frameWidth)
+cap.set(4, frameHeight)
+cap.set(10, 150)
+
+while True:
+
+
+    success, img = cap.read()
+
+    imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+
+    h_min = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    h_max = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    s_min = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    s_max = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    v_min = cv2.getTrackbarPos("Val Min", "TrackBars")
+    v_max = cv2.getTrackbarPos("Val Max", "TrackBars")
+    print(h_min, h_max, s_min, s_max, v_min, v_max)
+    lower = np.array([h_min, s_min, v_min])
+    upper = np.array([h_max, s_max, v_max])
+    mask = cv2.inRange(imgHSV, lower, upper)
+    imgResult = cv2.bitwise_and(img, img, mask=mask)
+
+    imgResult[mask > 0] = (0,0,230)
+    imgResult[mask == 0] = img[mask==0]
+
+
+
+
+    dst = cv2.addWeighted(imgResult, alpha, img, beta, 0.0)
+
+
+    imgStack = stackImages(0.6, ([img, imgHSV], [mask, dst]))
+    cv2.imshow("Stacked Images", imgStack)
+    cv2.waitKey(1)
+
